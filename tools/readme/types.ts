8e74467323b2c97bc9878042c689302772167beb
Alex Ramsdell <alexramsdell@gmail.com>
2020-02-07 18:08:40 -0500

    cosmetic.

--- tools/readme/types.ts
+++ tools/readme/types.ts
@@ -6,10 +6,12 @@ export interface Content {
   header: string;
   content: BlockContent;
 };
+
 export interface Code {
   type: 'code';
   content: BlockContent;
 };
+
 export type Block = Code | Content;
 
 export type IndexedBlocks = Map<string, Content[]>;
