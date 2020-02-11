c3eacfdd93dace688096e9c7d1d58d8c7b85d13d
Alex Ramsdell <alexramsdell@gmail.com>
2020-02-11 11:03:56 -0500

    rename test files, adapt test code.

--- tools/readme/test/readme.test.ts
+++ tools/readme/test/readme.test.ts
@@ -5,12 +5,12 @@ import { readFile } from 'fs';
 import 'mocha';
 import Readme from '../index';
 
-const testFiles = {
-  empty: './test/test-data/TEST1.md',
-  duplicateHeaders: './test/test-data/TEST2.md',
-  headersOnly: './test/test-data/TEST3.md',
-  codeBlocksContainingHeaders: './test/test-data/TEST4.md',
-  standard: './test/test-data/TEST5.md',
+const TEST_FILES = {
+  EMPTY: './test/test-data/empty.md',
+  DUPLICATE_HEADERS: './test/test-data/duplicateHeaders.md',
+  HEADERS_ONLY: './test/test-data/headersOnly.md',
+  CODE_BLOCKS_CONTAINING_HEADERS: './test/test-data/codeBlocksWithHeaders.md',
+  STANDARD: './test/test-data/standard.md',
 };
 
 describe('Readme constructor', () => {
@@ -29,28 +29,28 @@ describe('Readme constructor', () => {
 describe('Readme.parse', () => {
 
   it('empty file should only result in a _root block and a _root indexedBlock.', async() => {
-    const readme = await new Readme(testFiles.empty).parse();
+    const readme = await new Readme(TEST_FILES.EMPTY).parse();
     expect(readme.indexedBlocks.size === 1);
     expect(readme.blocks.length === 1);
   });
 
   it('6 headers (1 duplicate) should result in 7 blocks.', async() => {
-    const readme = await new Readme(testFiles.duplicateHeaders).parse();
+    const readme = await new Readme(TEST_FILES.DUPLICATE_HEADERS).parse();
     expect(readme.blocks.length).to.equal(7);
   });
 
   it('6 headers (1 duplicate) header should result in 6 indexedBlocks.', async() => {
-    const readme = await new Readme(testFiles.duplicateHeaders).parse();
+    const readme = await new Readme(TEST_FILES.DUPLICATE_HEADERS).parse();
     expect(readme.indexedBlocks.size).to.equal(6);
   });
 
   it('triplicate headers and no contents should result in 4 blocks (1 for root, 3 for each toc).', async() => {
-    const readme = await new Readme(testFiles.headersOnly).parse();
+    const readme = await new Readme(TEST_FILES.HEADERS_ONLY).parse();
     expect(readme.blocks.length).to.equal(4);
   });
 
   it('triplicate headers and no contents should result in 2 indexedBlocks (1 for root + 1 for toc).', async() => {
-    const readme = await new Readme(testFiles.headersOnly).parse();
+    const readme = await new Readme(TEST_FILES.HEADERS_ONLY).parse();
     expect(readme.indexedBlocks.size).to.equal(2);
   });
 
@@ -60,7 +60,7 @@ describe('Readme.getSection', async () => {
 
   it('should return null when a query doesnt match any heading', async () => {
 
-    const readme = await new Readme(testFiles.standard).parse();
+    const readme = await new Readme(TEST_FILES.STANDARD).parse();
     readme.setSection('Table of Contents', 'REPLACED CONTENT');
     const section = readme.getSection('Non-existent Heading');
     expect(section).to.be.null;
@@ -69,7 +69,7 @@ describe('Readme.getSection', async () => {
 
   it('should return null for any getSection call with an emtpy file', async () => {
 
-    const readme = await new Readme(testFiles.empty).parse();
+    const readme = await new Readme(TEST_FILES.EMPTY).parse();
     const nonExistentSection = readme.getSection('Table of Contents');
     expect(nonExistentSection).to.equal(null);
 
@@ -77,7 +77,7 @@ describe('Readme.getSection', async () => {
 
   it('should return value for an existing section using a RegExp', async () => {
 
-    const readme = await new Readme(testFiles.standard).parse();
+    const readme = await new Readme(TEST_FILES.STANDARD).parse();
     const section = readme.getSection(/^ *#+ *Table of Contents/);
     expect(section).to.have.property('content');
     expect(section).to.have.property('header');
@@ -90,7 +90,7 @@ describe('Readme.getSections', async () => {
 
   it('should return any emtpy array for any getSections call where there are no matches.', async () => {
 
-    const readme = await new Readme(testFiles.headersOnly).parse();
+    const readme = await new Readme(TEST_FILES.HEADERS_ONLY).parse();
     const sections = readme.getSections('Table of Contents');
     expect(sections.length).to.equal(3);
 
@@ -98,7 +98,7 @@ describe('Readme.getSections', async () => {
 
   it('should two entries for a duplicate header.', async () => {
 
-    const readme = await new Readme(testFiles.duplicateHeaders).parse();
+    const readme = await new Readme(TEST_FILES.DUPLICATE_HEADERS).parse();
     const sections = readme.getSections('Purpose');
     expect(sections.length).to.equal(2);
 
@@ -106,7 +106,7 @@ describe('Readme.getSections', async () => {
 
   it('should ignore content inside of code blocks', async() => {
 
-    const readme = await new Readme(testFiles.codeBlocksContainingHeaders).parse();
+    const readme = await new Readme(TEST_FILES.CODE_BLOCKS_CONTAINING_HEADERS).parse();
     const section = readme.getSection('Code Header');
     expect(section).to.be.null;
 
@@ -114,7 +114,7 @@ describe('Readme.getSections', async () => {
 
   it('should return value for an existing section using a RegExp', async () => {
 
-    const readme = await new Readme(testFiles.headersOnly).parse();
+    const readme = await new Readme(TEST_FILES.HEADERS_ONLY).parse();
     const sections = readme.getSections('Table of Contents');
     expect(sections.length).to.equal(3);
     expect(sections.every(({ header }) => header.includes('Table of Contents')));
@@ -128,7 +128,7 @@ describe('Readme.export', async () => {
 
   it('should return a section when Table of Contents is replaced with dummy data', async () => {
 
-    const readme = await new Readme(testFiles.standard).parse();
+    const readme = await new Readme(TEST_FILES.STANDARD).parse();
     const noModificationExport = readme.export(); 
     readme.setSection('Table of Contents', 'REPLACED CONTENT');
     expect(readme.getSection('Table of Contents')?.content.join('\n')).to.equal('REPLACED CONTENT');
@@ -142,10 +142,16 @@ describe('Readme.export', async () => {
 
   it('should export file correctly if it contains code blocks', async () => {
 
-    const readme = await new Readme(testFiles.codeBlocksContainingHeaders).parse();
-    const codeBlockFile = await promisify(readFile)(testFiles.codeBlocksContainingHeaders, 'utf8');
-    // ignoring differing white-space for now. should be fixed
-    expect(readme.export().split('\n').filter(Boolean).join('')).to.equal(codeBlockFile.split('\n').filter(Boolean).join(''))
+    const readme = await new Readme(TEST_FILES.CODE_BLOCKS_CONTAINING_HEADERS).parse();
+    const codeBlockFile = await promisify(readFile)(TEST_FILES.CODE_BLOCKS_CONTAINING_HEADERS, 'utf8');
+
+    // TODO: ignores differing white-space for now. fix.
+    expect(
+      readme.export()
+            .split('\n')
+            .filter(Boolean)
+            .join('')
+    ).to.equal(codeBlockFile.split('\n').filter(Boolean).join(''))
 
   });
 
