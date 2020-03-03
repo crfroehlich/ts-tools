d8c48dbc1aa6e6a68728b66b5bc7a67d02072e45
Alex Ramsdell <alexramsdell@gmail.com>
2020-03-03 15:20:01 -0500

    add optional content param to readme constructor to pass in readme as content

--- src/readme/readme.ts
+++ src/readme/readme.ts
@@ -1,8 +1,10 @@
 /* eslint-disable no-restricted-syntax */
+
 import { promisify } from 'util';
 import { readFile } from 'fs';
 import { resolve } from 'path';
 import { Block, IndexedBlocks, Query } from './types';
+
 
 /**
  * The Readme class represents a markdown README file and provides an API for programmatic transformations of it.
@@ -25,6 +27,10 @@ export class Readme {
     return `[${textParts.join(' ')}](#${Readme.sanitize(textParts.join('-')).toLowerCase()})`;
   };
 
+  private sourceIsFilepath(): boolean {
+    return Boolean(this.filepath);
+  }
+
   public static headerFound(header: string, query: Query, strict = false): boolean {
     // doesnt match case insensitive. should it?
 
@@ -43,15 +49,14 @@ export class Readme {
   }
 
   /*
-   * readmePath resolved relative to the calling code's cwd, resolved with 'path.resolve'.
+   * filepath resolved relative to the calling code's cwd, resolved with 'path.resolve'.
    */
-  readmePath: string;
+  private filepath?: string | null = null;
 
   /*
-   * 
+   * pass readme content in as a string instead of reading from a filepath. 
    */
-
-  content: string | null;
+  private content?: string | null = null;
 
   /*
    * A list of {@link Content} or {@link Code} blocks.
@@ -64,36 +69,36 @@ export class Readme {
   indexedBlocks: IndexedBlocks = new Map();
 
   /*
-   * @param readmePath - path to the readme file to be parsed.
-   *
+   * @param filepath - path to the readme file to be parsed.
+   * @param content - pass readme content in as a string instead of reading from a filepath. 
    */
-  constructor(readmePath: string | null, content: string = '') {
+  constructor({ filepath = null, content = null }) {
 
-    if (readmePath && content) {
-      throw new Error('Invalid Invocation: please pass either a string path or string content, but not both');
+    if (filepath && content || !(filepath && content)) {
+      throw new Error('Invalid Invocation: please pass either a string path or string content.');
     }
 
-    if (typeof readmePath === 'string' && readmePath.trim().length > 0) {
-      this.readmePath = resolve(readmePath);
-    } else {
-      throw new Error(`invalid path: ${readmePath}`);
-    }
+    this.content = content || null;
 
-    this.content = content; 
+    if (filepath === null) {
+      return;
+    }
+   
+    this.filepath = filepath || null;
 
   }
 
   /*
-   * Reads the readme at {@link readmePath} and returns it as a Promise-wrapped string.
+   * Reads the readme at {@link filepath} and returns it as a Promise-wrapped string.
    */
-  async getReadme(): Promise<string> {
+  private async getReadme(): Promise<string> {
 
     let content = '';
     try {
-      content = await promisify(readFile)(this.readmePath, { encoding: 'utf8' });
+      content = await promisify(readFile)(this.filepath || '', 'utf8');
     } catch (e) {
       if (e.code === 'ENOENT') {
-        process.stdout.write(`The file ${this.readmePath} could not be read.\n`);
+        process.stdout.write(`The file ${this.filepath} could not be read.\n`);
         process.exit(1);
       }
     }
@@ -103,11 +108,11 @@ export class Readme {
   }
 
   /*
-   * Parses the readme file at {@link readmePath} and returns it as Promise-wrapped Readme instance for chaining.
+   * Parses the readme file at {@link filepath} and returns it as Promise-wrapped Readme instance for chaining.
    */
   async parse(): Promise<Readme> {
-    const content = await this.getReadme();
-    const lines = content.split('\n');
+    const content = this.sourceIsFilepath() ? await this.getReadme() : this.content;
+    const lines = (content as string).split('\n');
 
     const rootBlock: Block = {
       header: '_root',
