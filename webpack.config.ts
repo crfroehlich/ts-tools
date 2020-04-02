75b51005da1d04f60cbeb280f42766325c14d17a
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-04-02 09:10:29 -0400

    Export compiled files individually so that they can be used directly by other projects.

--- webpack.config.ts
+++
@@ -1,8 +0,0 @@
-import { BundleTarget, getWebpackConfig } from '@ns8/protect-tools-bundle';
-
-export default getWebpackConfig({
-  bundleTarget: BundleTarget.NODE,
-  distDirectory: './dist',
-  sourceDirectory: './src/index.ts',
-  libraryName: 'protect-tools-js',
-});
