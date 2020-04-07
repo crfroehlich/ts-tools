ad6a4b9541a8905c7818149a6d763fbb2731e764
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-04-07 17:17:00 -0400

    Refactored the readme tooling to parse the entire project's markdown documentation.
    This eliminates the need to specify specific directories and allows the consumer to run all from the project root.

--- src/sortJson/sortJson.ts
+++ src/sortJson/sortJson.ts
@@ -1,15 +1,9 @@
 import glob from 'glob';
 import { readFileSync, writeFileSync } from 'fs';
 import { LogLevel, LogOutput, getLogger } from '../logger';
+import { GLOB_OPTIONS, GlobOptions, globCallback } from '../env/files';
 
 const sortedJson = require('sorted-json');
-
-type globCallback = (err: Error | null, matches: string[]) => void;
-export interface GlobOptions {
-  dot?: boolean;
-  ignore?: string[];
-  realPath?: boolean;
-}
 
 const log = getLogger(
   {
@@ -22,11 +16,6 @@ const log = getLogger(
 
 // Sort all the JSON files to improve readability and reduce conflicts
 const defaultPath = '**/*.json';
-const defaultOptions = {
-  dot: true,
-  ignore: ['**/node_modules/**', '.vscode/**', '**/*/tsconfig.json', '**/*nyc_output/**'],
-  realPath: true,
-};
 
 const defaultCallback = (er: Error | null, files: string[]): void => {
   if (er) {
@@ -54,7 +43,7 @@ const defaultCallback = (er: Error | null, files: string[]): void => {
 
 export const sortJson = (
   path: string = defaultPath,
-  options: GlobOptions = defaultOptions,
+  options: GlobOptions = GLOB_OPTIONS,
   callback: globCallback = defaultCallback,
 ): void => {
   glob(path, options, callback);
