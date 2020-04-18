49033fdc40efdf362eacd9235a3df294672cd07f
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-04-18 10:29:21 -0400

    Support deploying the API docs to GitHub pages automatically.

--- src/docs/apiDocs.ts
+++ src/docs/apiDocs.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { execSync } from 'child_process';
 const prompts = require('prompts');
+const ghpages = require('gh-pages');
 
 /**
  * Generates API docs for every documented class/method/interface/enum
@@ -8,25 +9,42 @@ const prompts = require('prompts');
  */
 export const generateApiDocs = async (params?: string): Promise<void> => {
 
-  // Get any command line arguments that might have been passed
-  const args = process.argv.slice(2);
+  let command = `api-documenter markdown --input-folder temp --output-folder api`;
+  if (params) {
+    command += ` ${params}`;
+  }
+  const cwd = `${process.cwd()}`;
+  console.info(`Running ${command} in ${cwd}`);
 
-  const processCommand = () => {
+  try {
+    execSync(command, { cwd, stdio: 'inherit' });
+  } catch (error) {
+    console.error(error);
+  }
 
-    let command = `api-documenter markdown --input-folder temp --output-folder api`;
-    if (params) {
-      command += ` ${params}`;
+  const publishApiDocs = () => {
+    ghpages.publish('api', (err: Error) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+
+  // If we're not in CI, allow the user to confirm before they deploy
+  if (!process.env.CI) {
+    const confirm = await prompts({
+      type: 'confirm',
+      name: 'yesno',
+      message: `You are about to to publish the API documentation to GitHub Pages'. Are you sure? Y/n`,
+    });
+    if (confirm.yesno) {
+      publishApiDocs();
+    } else {
+      console.log('User cancelled operation');
     }
-    const cwd = `${process.cwd()}`;
-    console.info(`Running ${command} in ${cwd}`);
-
-    try {
-      execSync(command, { cwd, stdio: 'inherit' });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  processCommand();
+  } else {
+    publishApiDocs();
+  }
 };
 
 if (__filename === process?.mainModule?.filename) {
