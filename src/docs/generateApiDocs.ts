25fb55f676b1b280d5b76b6b955f7d9ca45256d7
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-04-21 11:33:18 -0400

    Since this script is not bundled with other script executions, we can call it manually now.

--- src/docs/generateApiDocs.ts
+++ src/docs/generateApiDocs.ts
@@ -18,45 +18,43 @@ const log = getLogger();
  * @returns async void
  */
 export const generateApiDocs = async (params?: string): Promise<void> => {
-  // If we're in CI, generate and publish the docs
-  if (process.env.CI) {
-    let command = 'api-documenter markdown --input-folder temp --output-folder api';
-    if (params) {
-      command += ` ${params}`;
-    }
-    const cwd = `${process.cwd()}`;
-    log.info(`Running ${command} in ${cwd}`);
+  let command = 'api-documenter markdown --input-folder temp --output-folder api';
+  if (params) {
+    command += ` ${params}`;
+  }
+  const cwd = `${process.cwd()}`;
+  log.info(`Running ${command} in ${cwd}`);
 
-    try {
-      execSync(command, { cwd, stdio: 'inherit' });
-    } catch (error) {
-      log.error(error);
-    }
+  try {
+    execSync(command, { cwd, stdio: 'inherit' });
+  } catch (error) {
+    log.error(error);
+  }
 
-    const publish = (): any => {
-      // Publish to the `gh-pages` branch
-      return ghpages.publish('api', (err: Error) => {
-        if (err) {
-          log.error('Failed', err);
-        }
-      });
-    };
-
-    // If we're not in CI, allow the user to confirm before they deploy
-    if (!process.env.CI) {
-      const confirm = await prompts({
-        type: 'confirm',
-        name: 'yesno',
-        message: 'You are about to to publish the API documentation to GitHub Pages. Are you sure? Y/n',
-      });
-      if (confirm.yesno) {
-        publish();
-      } else {
-        log.info('User cancelled operation');
+  const publish = (): any => {
+    // Publish to the `gh-pages` branch
+    return ghpages.publish('api', (err: Error) => {
+      if (err) {
+        log.error('Failed', err);
       }
-    } else {
+    });
+  };
+
+  // If we're not in CI, allow the user to confirm before they deploy
+  if (!process.env.CI) {
+    const confirm = await prompts({
+      type: 'confirm',
+      name: 'yesno',
+      message: 'You are about to to publish the API documentation to GitHub Pages. Are you sure? Y/n',
+    });
+    if (confirm.yesno) {
       publish();
+    } else {
+      log.info('User cancelled operation');
     }
+  } else {
+      // If we're in CI, generate and publish the docs
+    publish();
   }
 };
 
